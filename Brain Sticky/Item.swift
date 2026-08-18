374b14f534090d9a1c47b72fe112cff6a21be763
Xiaojing Ji <ritajixiaojing@gmail.com>
2026-08-17 23:51:40 -0700

    Initial Commit

---
+++ Brain Sticky/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Brain Sticky
+//
+//  Created by Xiaojing Ji on 8/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
